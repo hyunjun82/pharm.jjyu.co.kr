fe978a6bf8c1151a1f8a7045a48d88aa3072b6c1
Wiki Author <user@jjyu.co.kr>
2026-03-16 15:27:39 +0900

    Microsoft Clarity 설치 (vvy5qacjv6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,15 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
 
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "vvy5qacjv6");`}
+        </Script>
+
         {/* #5 WebSite 스키마 - 사이트 검색창 */}
         <script
           type="application/ld+json"
